33ef284e497eba9ef73450c3f3418ca067bde656
Michal Žamboch <zamboch.michal@seznam.cz>
2023-10-28 10:42:35 +0200

    Netwok update.

--- src/backend/app.rs
+++ src/backend/app.rs
@@ -1,22 +1,35 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports)]
 
-use sysinfo::{CpuExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 use std::cmp;
+use sysinfo::{CpuExt, NetworkData, NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
 use super::utils::get_floored_percentage;
 
 pub struct App {
     sys: System,
-    max_received_net: u64,
-    max_transmited_net: u64,
+    max_received_net: Vec<u64>,
+    max_transmited_net: Vec<u64>,
 }
 
 impl App {
     pub fn new() -> App {
         let mut system = System::new();
         system.refresh_all();
+        let network_count = App::get_network_count(&mut system);
 
-        App { sys: system, max_received_net: 0, max_transmited_net: 0 }
+        App {
+            sys: system,
+            max_received_net: vec![0; network_count],
+            max_transmited_net: vec![0; network_count],
+        }
+    }
+
+    fn get_network_count(sys: &mut System) -> usize {
+        sys.refresh_networks();
+        sys.refresh_networks_list();
+        
+        let net = sys.networks();
+        net.into_iter().count()
     }
 
     pub fn on_tick(&mut self) {
@@ -46,20 +59,32 @@ impl App {
         usage as u64
     }
 
-    fn network(&self) {
-        println!("=> networks:");
-        for (interface_name, data) in self.sys.networks() {
-            println!(
-                "{}: {}/{} B",
-                interface_name,
-                data.received(),
-                data.transmitted()
-            );
+    pub fn network(&mut self) {
+        for (index, net_info) in self.sys.networks().into_iter().enumerate() {
+            self.max_received_net[index] = self.get_max_received(index, net_info.1);
+
+            self.max_transmited_net[index] = self.get_max_transmited(index, net_info.1);
         }
     }
 
-    fn update_net_info(&mut self) {
-        
+    fn get_max_received(&self, index: usize, net_info: &NetworkData) -> u64 {
+        let received = self.max_received_net[index];
+        cmp::max(received, net_info.received())
+    }
+
+    fn get_max_transmited(&self, index: usize, net_info: &NetworkData) -> u64 {
+        let transmitted = self.max_transmited_net[index];
+        cmp::max(transmitted, net_info.transmitted())
+    }
+
+    fn print_net_info(&self, index: usize, net_info: (&String, &NetworkData)) {
+        println!(
+            "{} {}: {}/{} B",
+            index,
+            net_info.0,
+            net_info.1.received(),
+            net_info.1.transmitted()
+        );
     }
 
     pub fn get_process_info(&self) {
