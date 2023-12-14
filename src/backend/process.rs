a526aa9a4c3145fbae36644f7ec88da9e24efdb7
Michal Žamboch <zamboch.michal@seznam.cz>
2023-12-14 19:57:38 +0100

    Process backend clean up.

--- src/backend/process.rs
+++ src/backend/process.rs
@@ -43,75 +43,12 @@ impl IProcessStringView for ProcessItem {
     }
 }
 
-fn new_process_item(pid: Pid, proc: &Process) -> ProcessItem {
-    ProcessItem {
-        pid,
-        name: proc.name().to_owned(),
-        cpu_usage: proc.cpu_usage() as u64,
-        memory_usage: proc.memory(),
-        disk_read_usage: proc.disk_usage().read_bytes,
-        disk_write_usage: proc.disk_usage().written_bytes,
-    }
-}
-
-// TEST -----------------------------------------------------
-
-fn processes_boxed_strings_sorted_by(sys: &System, sort_by: SortBy) -> Box<[Box<[String]>]> {
-    boxed_processes_sorted_by(sys, sort_by)
-        .par_iter()
-        .map(process_into_string_box_arr)
-        .collect()
-}
-
-fn processes_boxed_slices_sorted_by() -> Box<[Box<[&'static str]>]> {
-    Box::new([])
-}
-
-fn all_processes_boxed_slices_sorted_by(sys: &System, sort_by: SortBy) -> Box<[[String; 6]]> {
-    boxed_processes_sorted_by(sys, sort_by)
-        .par_iter()
-        .map(process_into_string_arr)
-        .collect()
-}
-
-fn all_processes_strings_arr_sorted_by(sys: &System, sort_by: SortBy) -> Vec<[String; 6]> {
-    boxed_processes_sorted_by(sys, sort_by)
-        .par_iter()
-        .map(process_into_string_arr)
-        .collect()
-}
-
-pub fn process_into_string_arr(item: &ProcessItem) -> [String; 6] {
-    [
-        item.get_pid(),
-        item.get_name(),
-        item.get_cpu_usage(),
-        item.get_memory_usage(),
-        item.get_disk_read_usage(),
-        item.get_disk_write_usage(),
-    ]
-}
-
-fn process_into_string_box_arr(item: &ProcessItem) -> Box<[String]> {
-    [
-        item.get_pid(),
-        item.get_name(),
-        item.get_cpu_usage(),
-        item.get_memory_usage(),
-        item.get_disk_read_usage(),
-        item.get_disk_write_usage(),
-    ]
-    .into()
-}
-
-fn arc_processes_sorted_by(sys: &System, sort_by: SortBy) -> Arc<[ProcessItem]> {
+pub fn arc_processes_sorted_by(sys: &System, sort_by: SortBy) -> Arc<[ProcessItem]> {
     let mut processes = processes_into_boxed_items(sys);
     sort_processes_by(&mut processes, sort_by);
 
     processes.into()
 }
-
-// TEST -----------------------------------------------------
 
 pub fn boxed_processes_sorted_by(sys: &System, sort_by: SortBy) -> Box<[ProcessItem]> {
     let mut processes = processes_into_boxed_items(sys);
@@ -125,6 +62,17 @@ fn processes_into_boxed_items(sys: &System) -> Box<[ProcessItem]> {
         .par_iter()
         .map(|(pid, proc)| new_process_item(*pid, proc))
         .collect()
+}
+
+fn new_process_item(pid: Pid, proc: &Process) -> ProcessItem {
+    ProcessItem {
+        pid,
+        name: proc.name().to_owned(),
+        cpu_usage: proc.cpu_usage() as u64,
+        memory_usage: proc.memory(),
+        disk_read_usage: proc.disk_usage().read_bytes,
+        disk_write_usage: proc.disk_usage().written_bytes,
+    }
 }
 
 fn sort_processes_by(processes: &mut [ProcessItem], sort_by: SortBy) {
