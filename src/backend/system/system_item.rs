b59d671cc876653aafe6af0cf762605783b8c2bb
Michal Žamboch <zamboch.michal@seznam.cz>
2024-01-01 20:59:03 +0100

    system item update

--- src/backend/system/system_item.rs
+++ src/backend/system/system_item.rs
@@ -1,31 +1,42 @@
 #![allow(dead_code)]
 
-use std::{
-    cell::RefCell,
-    time::{Duration, Instant},
-};
+use std::{cell::RefCell, rc::Rc, time::*};
 
 #[derive(Debug)]
-pub struct SystemItem {
+pub struct SystemItem<T> {
     duration: Duration,
     last_tick: RefCell<Instant>,
+    item: RefCell<Rc<T>>,
 }
 
-impl SystemItem {
-    pub fn new(millis: u64) -> Self {
+impl<T> SystemItem<T> {
+    pub fn new(millis: u64, value_getter: &dyn Fn() -> T) -> Self {
+        let value = value_getter();
+
         Self {
             duration: Duration::from_secs(millis),
             last_tick: RefCell::new(Instant::now()),
+            item: RefCell::new(Rc::new(value)),
         }
     }
 
-    pub fn update(&self) -> bool {
-        if self.last_tick.borrow().elapsed() >= self.duration {
+    pub fn update(&self, value_getter: &dyn Fn() -> T) {
+        if self.time_to_update() {
+            self.set(value_getter);
             *self.last_tick.borrow_mut() = Instant::now();
-
-            return true;
         }
+    }
 
-        false
+    pub fn get(&self) -> Rc<T> {
+        self.item.borrow().clone()
+    }
+
+    fn time_to_update(&self) -> bool {
+        self.last_tick.borrow().elapsed() >= self.duration
+    }
+
+    fn set(&self, value_getter: &dyn Fn() -> T) {
+        let value = value_getter();
+        *self.item.borrow_mut() = Rc::new(value);
     }
 }
